e332f60c5a8ea95fb2e2ac1cc99b6c5ee68bb74d
N-Adkins <noahcadkins@gmail.com>
2022-11-29 12:59:02 -0500

    Added automatic packer installation

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,10 +1,25 @@
+-- Autoinstall packer if it isn't already
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
 	--Packer manages itself
 	use 'wbthomason/packer.nvim'
-	
+
 	-- Theming
 	use 'folke/tokyonight.nvim'
-    use 'morhetz/gruvbox' 
+    use 'morhetz/gruvbox'
 
 	-- CMP
 	use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" }
@@ -19,11 +34,11 @@ return require('packer').startup(function(use)
     use 'honza/vim-snippets'
 	use 'dcampos/nvim-snippy'
     use 'dcampos/cmp-snippy'
-	
+
 	-- LSP
 	use 'neovim/nvim-lspconfig'
-	use 'williamboman/mason.nvim' 
-    
+	use 'williamboman/mason.nvim'
+
     -- Telescope
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
@@ -34,7 +49,7 @@ return require('packer').startup(function(use)
 
     -- Treesitter
     use 'nvim-treesitter/nvim-treesitter'
-    
+
     -- Icons
     use 'nvim-tree/nvim-web-devicons'
     use 'lewis6991/gitsigns.nvim'
@@ -42,12 +57,16 @@ return require('packer').startup(function(use)
     -- Status Bar
     use 'nvim-lualine/lualine.nvim'
     use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-    
+
     -- CMake
     use 'cdelledonne/vim-cmake'
-    
+
     -- Whitespace
     use 'ntpeters/vim-better-whitespace'
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 
 end)
 
