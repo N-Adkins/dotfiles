7908efcdabf349fe71f98058ad76f0f9dcda544e
N-Adkins <noahcadkins@gmail.com>
2022-12-03 11:47:08 -0500

    Added diagnostics popup for Neovim

--- .config/nvim/lua/mason_config.lua
+++ .config/nvim/lua/mason_config.lua
@@ -6,3 +6,14 @@ require('lspconfig').pyright.setup{}
 require('lspconfig').tsserver.setup{}
 
 require('rust-tools').setup()
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		virtual_text = false,
+		underline = true,
+		signs = true,
+	}
+)
+
+vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float({scope="line"})]]
+vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
